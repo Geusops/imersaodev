7fa85e56a1a0e342eca7bd2516cea3a993e28189
unknown <geuso002@gmail.com>
2021-04-01 20:11:51 -0300

    New update

--- conv.js
+++ conv.js
@@ -5,15 +5,18 @@ var valorEmHexadecimal
 //Funcoes para as conversoes
 //conversao para de Decimal para Hexadecimal
 function convDecimal(){
+  //conversao para int
   numDec = parseInt(document.getElementById('decnum').value);
+  //conversao para hexa
   valorEmHexadecimal = numDec.toString(16);
-  document.getElementById('hexnum').value = valorEmHexadecimal;
+  //impressao do valor
+  document.getElementById('resultado').innerHTML = "O número em Hexadecimal é: " + valorEmHexadecimal;
 }
 
 //limpeza dos campos
 function limpar(){
  document.getElementById('decnum').value = "";
- document.getElementById('hexnum').value = "";
 }
 
 //setAttribute('value', valorEmHexadecimal)
+
